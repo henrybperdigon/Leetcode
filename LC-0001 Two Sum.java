127f825593076d4ac4747d8c5439607828d7304b
Henry Perdigon <henrybperdigon@yahoo.com>
2023-09-07 16:22:18 -0700

    Update LC-0001 Two Sum.java

--- LC-0001 Two Sum.java
+++ LC-0001 Two Sum.java
@@ -14,6 +14,12 @@ class Solution {
 }
 
 /**
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 BRUTE FORCE - Iterate for all possible pairs and if the sum is 9 return their indices (On*n)
 
 Iterate over array getting the compliment by subracting the target with the element
