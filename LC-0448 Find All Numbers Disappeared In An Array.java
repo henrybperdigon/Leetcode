8e70042d1f24c25b3182556c83d4e1f8440afa0a
Henry Perdigon <henrybperdigon@yahoo.com>
2023-09-08 01:54:26 -0700

    Update LC-0448 Find All Numbers Disappeared In An Array.java

--- LC-0448 Find All Numbers Disappeared In An Array.java
+++ LC-0448 Find All Numbers Disappeared In An Array.java
@@ -1,3 +1,29 @@
+/**
+Approach 1: Using HashMap
+*/
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int num : nums) {
+            map.put(num-1, map.getOrDefault(0,1));
+        }
+
+        for (int i=0; i<nums.length; i++) {
+            if (!map.containsKey(i)) {
+                result.add(i+1);
+            }
+        }
+
+        return result;
+        
+    }
+}
+
+/**
+Approach 1: Using In-Place Array Manipulation
+*/
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         for (int i=0; i<nums.length; i++) {
