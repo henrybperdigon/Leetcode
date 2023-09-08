a1c6cc3019d107a7ef3f4e8cdd431a263ea58ac6
Henry Perdigon <henrybperdigon@yahoo.com>
2023-09-08 01:16:55 -0700

    Update LC-0268 Missing Number.java

--- LC-0268 Missing Number.java
+++ LC-0268 Missing Number.java
@@ -13,3 +13,23 @@ class Solution {
         return nums.length;
     }
 }
+
+/**
+Approach 2: Sum Difference
+*/
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int expectedSum = 0;
+        for (int i=0; i<=nums.length; i++) {
+            expectedSum +=i;
+        }
+
+        int actualSum = 0;
+        for (int num: nums) {
+            actualSum += num;
+        }
+
+        return expectedSum-actualSum;
+    }
+}
