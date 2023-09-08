374134f18d41b79640ede99719c9edb0f0642559
Henry Perdigon <henrybperdigon@yahoo.com>
2023-09-07 23:42:16 -0700

    Create LC-0189 Rotate Array.java

---
+++ LC-0189 Rotate Array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k%nums.length;
+        int start = 0;
+        int end = nums.length-1;
+
+        reverse(nums, start, end);
+        reverse(nums, start, k-1);
+        reverse(nums, k, end);
+    }
+
+     public void reverse(int[] nums, int start, int end) {
+            while (start<end) {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+    }
+}
